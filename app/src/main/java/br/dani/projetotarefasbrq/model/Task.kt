7fcf9d02338d43566077905750509da0418a70a6
Fabiano Barbosa <77905346+fabibarbosa@users.noreply.github.com>
2021-05-14 17:12:00 -0300

    Atualização  da tela de tarefas

--- app/src/main/java/br/dani/projetotarefasbrq/model/Task.kt
+++ app/src/main/java/br/dani/projetotarefasbrq/model/Task.kt
@@ -1,7 +1,13 @@
 package br.dani.projetotarefasbrq.model
 
-class Task (var title : String, var description : String, var status : Boolean, var priority: String){
+import java.util.*
+
+class Task (var title : String, var description : String, var priority: String){
+    private var status: Boolean
+    private lateinit var date : Date
+    var dateFormat = ("dd/M/yyyy")
     init {
+        var date = dateFormat.format(Date())
         this.status = false
     }
 
