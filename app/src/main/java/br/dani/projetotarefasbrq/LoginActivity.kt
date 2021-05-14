512218db20877ff2b39b2bb89c8822e2a9fae13d
Daniele de Souza Ferreira <danieledesouzaferreira88@gmail.com>
2021-05-14 17:36:26 -0300

    adicionado toolbar

--- app/src/main/java/br/dani/projetotarefasbrq/LoginActivity.kt
+++ app/src/main/java/br/dani/projetotarefasbrq/LoginActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.Toolbar
 
 class LoginActivity : AppCompatActivity() {
 
@@ -12,7 +13,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-   
+   val toolbar = findViewById<Toolbar>(R.id.toolbar)
 
     }
 
